43772b762cc2c8f001602bdd4b6278eb7e56f3a4
khatereh <khaterehnaseri444@gmail.com>
2026-04-27 09:43:04 +0330

    style:adjust inputs in 3 columns

--- todo/src/components/done/Done.jsx
+++ todo/src/components/done/Done.jsx
@@ -7,9 +7,11 @@ function Done() {
   const { filterByCondition } = useTodo();
   const tasks = filterByCondition.done;
   return (
-    <div className="w-95 h-auto min-h-118 bg-fuchsia-100 p-3 flex flex-col items-center justify-center">
-      <h1>Done Tasks</h1>
-      <AddTask currentCondition={"done"} />
+    <div className="w-95 h-auto min-h-118 bg-fuchsia-100 p-3 flex flex-col items-center justify-center relative">
+      <div className="w-full h-40 flex items-center justify-center flex-col absolute top-2.5">
+        <h1 className="text-[20px] font-extralight">Done Tasks</h1>
+        <AddTask currentCondition={"done"} />
+      </div>
       {tasks.map((T) => (
         <React.Fragment key={T.id}>
           <TaskCard task={T} />
--- todo/src/components/inprogress/Inprogress.jsx
+++ todo/src/components/inprogress/Inprogress.jsx
@@ -7,9 +7,11 @@ function Inprogress() {
   const { filterByCondition } = useTodo();
   const tasks = filterByCondition.inprogress;
   return (
-    <div className="w-95 h-auto min-h-118 p-3 bg-fuchsia-100 flex flex-col items-center justify-center">
-      <h1>In Progress</h1>
-      <AddTask currentCondition={"inprogress"} />
+    <div className="w-95 h-auto min-h-118 p-3 bg-fuchsia-100 flex flex-col items-center justify-center relative">
+      <div className="w-full h-40 flex items-center justify-center flex-col absolute top-2.5">
+        <h1 className="text-[20px] font-extralight">In Progress</h1>
+        <AddTask currentCondition={"inprogress"} />
+      </div>
       {tasks.map((T) => (
         <React.Fragment key={T.id}>
           <TaskCard task={T} />
