42c1d2f129e27c52fb825126247f3cbd23312217
khatereh <khaterehnaseri444@gmail.com>
2026-04-28 13:03:07 +0330

    style:test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="w-full h-15 bg-fuchsia-400 flex items-center justify-center">
+    <div className="w-full h-15 lg:bg-fuchsia-400 flex md:bg-green-400 items-center justify-center">
       <h1 className="text-[30px] font-extralight text-white">Todo List</h1>
     </div>
   );
