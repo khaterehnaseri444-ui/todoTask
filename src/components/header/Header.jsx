4e0bbae1990d2c8a868652890958b42c96e799ab
khatereh <khaterehnaseri444@gmail.com>
2026-04-28 15:18:28 +0330

    install router-dom

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
+const navBar = [
+  { id: 1, title: "All" },
+  { id: 2, title: "Done" },
+  { id: 3, title: "In progress" },
+  { id: 4, title: "Todos" },
+];
+
 function Header() {
   return (
-    <div className="w-full lg:h-15 md:h-12 bg-fuchsia-400 flex items-center justify-center">
-      <h1 className="lg:text-[30px] md:text-[25px] font-extralight text-white">Todo List</h1>
+    <div className="w-full lg:h-18 md:h-12 bg-fuchsia-400 flex flex-col items-center justify-center">
+      <h1 className="lg:text-[15px] lg:h-6 md:text-[25px] flex items-center justify-center font-extralight text-white">
+        Your Todo List
+      </h1>
+      <div className="lg:w-full lg:h-13 bg-lime-300"></div>
     </div>
   );
 }
