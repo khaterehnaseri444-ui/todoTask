4e067489e86e64c8f770a593b4e27f6e4e59aa93
khatereh <khaterehnaseri444@gmail.com>
2026-04-29 09:50:59 +0330

    style:complete header's style in tablet mode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,16 +10,16 @@ const navBar = [
 
 function Header() {
   return (
-    <div className="w-full lg:h-18 md:h-12 flex flex-col items-center justify-center">
-      <h1 className="w-full bg-fuchsia-100 lg:text-[15px] lg:h-6 md:text-[25px] flex items-center justify-center font-extralight text-fuchsia-500">
+    <div className="w-full lg:h-18 md:h-12 flex flex-col items-center justify-center bg-lime-200">
+      <h1 className="w-full bg-fuchsia-100 lg:text-[15px] md:text-[10px] lg:h-6 flex items-center justify-center font-extralight text-fuchsia-500">
         Your Todo List
       </h1>
-      <div className="w-full h-13 flex items-center justify-center bg-fuchsia-400">
-        <div className="lg:w-300 lg:h-12 md:hidden lg:flex items-center justify-start gap-16">
+      <div className="w-full lg:h-13 md:h-9 flex items-center justify-center bg-fuchsia-400">
+        <div className="lg:w-300 lg:h-12 md:h-6 flex items-center justify-start gap-16">
           {navBar.map((item) => (
             <React.Fragment key={item.id}>
               <Link to={item.path}>
-                <p className="text-white font-extralight cursor-pointer hover:text-fuchsia-700">
+                <p className="text-white md:text-[14px] font-extralight cursor-pointer hover:text-fuchsia-700">
                   {item.title}
                 </p>
               </Link>
